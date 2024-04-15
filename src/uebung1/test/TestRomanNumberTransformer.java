aee8716ca7707e78134470d1ad868e22d3e377ec
Funky <Funky@localhost>
2024-04-15 17:53:57 +0200

    1.1c

--- src/uebung1/test/TestRomanNumberTransformer.java
+++ src/uebung1/test/TestRomanNumberTransformer.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 import uebung1.buisnesslogic.RomanNumberTransformer;
 import uebung1.buisnesslogic.TransformerFactory;
 
+import static java.lang.Float.NaN;
+
 class TestRomanNumberTransformer {
 
     RomanNumberTransformer transf;
-
+    private final String error = "Fehler: ungültiger Wertebereich";
     @BeforeEach
     void init() {
         transf = TransformerFactory.getRomanNumberTransformer();
@@ -18,11 +20,15 @@ class TestRomanNumberTransformer {
     @Test
     void TestRomanNumbers() {
 
-//        GermanFormatNumberTransformer ger = new GermanFormatNumberTransformer();
-//        String res = ger.transformNumber(1234);
-//        System.out.println(res);
-        String error = "Fehler: ungültiger Wertebereich";
+        uAek1(); //gültige Äquivalenzklasse1
+        uAek2(); //ungültige Äquivalenzklasse2
+        // uAek3(); //ungültige Äquivalenzklasse3; Testcase 10 mit "ABC" -> Compilerfehler
+        uAek4(); //ungültige Äquivalenzklasse4
+    }
 
+
+
+    private void uAek1() {
         //Testcode
         //1. Äquivalenzklasse
         //Testcase 1
@@ -31,22 +37,69 @@ class TestRomanNumberTransformer {
         Assertions.assertEquals("MD", transf.transformNumber(1500));
         //Testcase 3
         Assertions.assertEquals("MMM", transf.transformNumber(3000));
-
-        //2. Äquivalenzklasse
         //Testcase 4
-        Assertions.assertEquals(error, transf.transformNumber(0));
+        Assertions.assertEquals("I", transf.transformNumber(1));
         //Testcase 5
-        Assertions.assertEquals(error, transf.transformNumber(-23453));
+        Assertions.assertEquals("II", transf.transformNumber(2));
         //Testcase 6
-        Assertions.assertEquals(error, transf.transformNumber(Integer.MIN_VALUE));
-
-        //3. Äquivalenzklasse
+        Assertions.assertEquals("III", transf.transformNumber(3));
         //Testcase 7
-        Assertions.assertEquals(error, transf.transformNumber(3001));
+        Assertions.assertEquals("IV", transf.transformNumber(4));
         //Testcase 8
-        Assertions.assertEquals(error, transf.transformNumber(345632));
+        Assertions.assertEquals("V", transf.transformNumber(5));
         //Testcase 9
-        Assertions.assertEquals(error, transf.transformNumber(Integer.MAX_VALUE));
+        Assertions.assertEquals("VI", transf.transformNumber(6));
+        //Testcase 10
+        Assertions.assertEquals("VII", transf.transformNumber(7));
+        //Testcase 11
+        Assertions.assertEquals("VIII", transf.transformNumber(8));
+        //Testcase 12
+        Assertions.assertEquals("IX", transf.transformNumber(9));
+        //Testcase 13
+        Assertions.assertEquals("X", transf.transformNumber(10));
+        //Testcase 14
+        Assertions.assertEquals("XL", transf.transformNumber(40));
+        //Testcase 15
+        Assertions.assertEquals("L", transf.transformNumber(50));
+        //Testcase 16
+        Assertions.assertEquals("XC", transf.transformNumber(90));
+        //Testcase 17
+        Assertions.assertEquals("C", transf.transformNumber(100));
+        //Testcase 18
+        Assertions.assertEquals("CD", transf.transformNumber(400));
+        //Testcase 19
+        Assertions.assertEquals("D", transf.transformNumber(500));
+        //Testcase 20
+        Assertions.assertEquals("CM", transf.transformNumber(900));
+        //Testcase 21
+        Assertions.assertEquals("M", transf.transformNumber(1000));
+
     }
 
+    private void uAek2() {
+        //2. Äquivalenzklasse
+        //Testcase 22
+        Assertions.assertEquals(error, transf.transformNumber(0));
+        //Testcase 23
+        Assertions.assertEquals(error, transf.transformNumber(-23453));
+        //Testcase 24
+        Assertions.assertEquals(error, transf.transformNumber(Integer.MIN_VALUE));
+    }
+
+    private void uAek3(){
+        // 3. Äquivalenzklasse
+        // Testcase 25
+        // Assertions.assertEquals(NaN, transf.transformNumber(("ABC"));
+
+    }
+
+    private void uAek4(){
+        //4. Äquivalenzklasse
+        //Testcase 26
+        Assertions.assertEquals(error, transf.transformNumber(3001));
+        //Testcase 27
+        Assertions.assertEquals(error, transf.transformNumber(345632));
+        //Testcase 28
+        Assertions.assertEquals(error, transf.transformNumber(Integer.MAX_VALUE));
+    }
 }
